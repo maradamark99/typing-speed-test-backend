0ba1801a97238b2e63022d928bbed8adfc91a50b
Mark Marada <maradamark99@gmail.com>
2022-04-12 11:10:07 +0200

    Update ResultController.java

--- src/main/java/com/mmark09/typingspeedtest/result/ResultController.java
+++ src/main/java/com/mmark09/typingspeedtest/result/ResultController.java
@@ -1,6 +1,7 @@
 package com.mmark09.typingspeedtest.result;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "localhost:4200") // for now, this will do
 @RequestMapping(path = "api/v1/results")
 @AllArgsConstructor
 public class ResultController {
@@ -18,6 +20,7 @@ public class ResultController {
     public Result save(Result result) {
         return resultRepository.save(result);
     }
+
     @GetMapping
     public List<Result> getResults() {
         return resultRepository.findAll();
