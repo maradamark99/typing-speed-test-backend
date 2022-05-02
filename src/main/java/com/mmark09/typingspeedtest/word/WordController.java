7515511e56edb2cb7dd1ac2719c47eb0e3092cec
Mark Marada <maradamark99@gmail.com>
2022-05-02 16:26:59 +0200

    Add getDifficulties in WordController

--- src/main/java/com/mmark09/typingspeedtest/word/WordController.java
+++ src/main/java/com/mmark09/typingspeedtest/word/WordController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -18,6 +19,10 @@ public class WordController {
 
     private final WordService wordService;
 
+    @GetMapping("/difficulties")
+    public ResponseEntity<List<Difficulty>> getDifficulties() {
+        return ResponseEntity.ok().body(Arrays.asList(Difficulty.values()));
+    }
     @GetMapping("/{difficulty}")
     public ResponseEntity<List<Word>> getAllByDifficulty(@PathVariable("difficulty") String difficulty) {
         try{
