d12fed2d6a8dc28b0ed0a9a91edacf87b85582c3
maradamark99 <maradamark99@gmail.com>
2022-12-16 17:48:40 +0100

    Create Difficuly and Word entities

---
+++ src/main/java/com/maradamark09/typingspeedtest/word/Word.java
@@ -0,0 +1,32 @@
+package com.maradamark09.typingspeedtest.word;
+
+import com.maradamark09.typingspeedtest.difficulty.Difficulty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "words")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Word {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String value;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Difficulty difficulty;
+
+}
