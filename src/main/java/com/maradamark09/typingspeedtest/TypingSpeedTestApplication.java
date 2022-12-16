c282c51d6b047980478c4f5cc1ae61e23fdb50f3
maradamark99 <maradamark99@gmail.com>
2022-12-16 17:35:49 +0100

    Initialize project

---
+++ src/main/java/com/maradamark09/typingspeedtest/TypingSpeedTestApplication.java
@@ -0,0 +1,13 @@
+package com.maradamark09.typingspeedtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypingSpeedTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypingSpeedTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maradamark09/typingspeedtest/TypingSpeedTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maradamark09.typingspeedtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypingSpeedTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
