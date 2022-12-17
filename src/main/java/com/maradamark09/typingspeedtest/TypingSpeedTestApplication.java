6b17ebe64a5e77c2ccde9c366547043f4c2d8072
maradamark99 <maradamark99@gmail.com>
2022-12-17 15:38:13 +0100

    Disable spring security auto config

--- src/main/java/com/maradamark09/typingspeedtest/TypingSpeedTestApplication.java
+++ src/main/java/com/maradamark09/typingspeedtest/TypingSpeedTestApplication.java
@@ -2,8 +2,9 @@ package com.maradamark09.typingspeedtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TypingSpeedTestApplication {
 
 	public static void main(String[] args) {
