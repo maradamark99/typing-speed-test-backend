c727d5efca2f00abe09aa3e4d9be567d92f83398
maradamark99 <maradamark99@gmail.com>
2022-12-17 14:25:05 +0100

    Create a global exception handler

---
+++ src/main/java/com/maradamark09/typingspeedtest/exception/RestControllerExceptionHandler.java
@@ -0,0 +1,69 @@
+package com.maradamark09.typingspeedtest.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@RestControllerAdvice
+public class RestControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public MyErrorResponse handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        return new MyErrorResponse(HttpStatus.NOT_FOUND,
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+    }
+
+    @ExceptionHandler({
+            HttpMessageNotReadableException.class,
+            MethodArgumentNotValidException.class,
+            MethodArgumentTypeMismatchException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MyErrorResponse handleBadRequestException(RuntimeException ex, WebRequest request){
+        return new MyErrorResponse(
+                HttpStatus.BAD_REQUEST,
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+    }
+
+    @ExceptionHandler(WordLengthGreaterThanDifficultyException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public MyErrorResponse handleWordLengthGreaterThanDifficultyException(
+            WordLengthGreaterThanDifficultyException ex, WebRequest request) {
+        return new MyErrorResponse(
+                HttpStatus.UNPROCESSABLE_ENTITY,
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+    }
+
+    @ExceptionHandler(ResourceAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public MyErrorResponse handleWordAlreadyExistsException(
+            ResourceAlreadyExistsException ex,
+            WebRequest request) {
+        return new MyErrorResponse(
+                HttpStatus.CONFLICT,
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public MyErrorResponse handleGlobalException(Exception ex, WebRequest request) {
+        return new MyErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+    }
+}
