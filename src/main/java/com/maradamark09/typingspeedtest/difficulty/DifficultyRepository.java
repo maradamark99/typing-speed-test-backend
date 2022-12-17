02757322a845c4676d0b6f1ddd4ac86311250bfb
maradamark99 <maradamark99@gmail.com>
2022-12-17 14:34:04 +0100

    Implement DifficultyService and update repository with existsByValue

--- src/main/java/com/maradamark09/typingspeedtest/difficulty/DifficultyRepository.java
+++ src/main/java/com/maradamark09/typingspeedtest/difficulty/DifficultyRepository.java
@@ -3,4 +3,6 @@ package com.maradamark09.typingspeedtest.difficulty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DifficultyRepository extends JpaRepository<Difficulty, Long> {
+    Boolean existsByValue(String difficulty);
+
 }
