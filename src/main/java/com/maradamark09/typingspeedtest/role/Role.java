e96c219f3a6c779dc2313fb333cb56d7875699da
maradamark99 <maradamark99@gmail.com>
2023-01-07 20:28:23 +0100

    Add a toString annotation

--- src/main/java/com/maradamark09/typingspeedtest/role/Role.java
+++ src/main/java/com/maradamark09/typingspeedtest/role/Role.java
@@ -4,11 +4,13 @@ package com.maradamark09.typingspeedtest.role;
 import com.maradamark09.typingspeedtest.user.User;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.ToString;
 
 import java.util.Set;
 
 @Entity
 @Table(name = "user_roles")
+@ToString(exclude = {"id", "users"})
 public class Role {
 
     @Id
