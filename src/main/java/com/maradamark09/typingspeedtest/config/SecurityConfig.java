43e3cd796c0816bc7a972a6c00d803c0d8c4118d
maradamark99 <maradamark99@gmail.com>
2023-01-17 14:38:25 +0100

    Add exception handler filter before JWTAuthFilter

--- src/main/java/com/maradamark09/typingspeedtest/config/SecurityConfig.java
+++ src/main/java/com/maradamark09/typingspeedtest/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.maradamark09.typingspeedtest.config;
 
+import com.maradamark09.typingspeedtest.exception.FilterChainExceptionHandler;
 import com.maradamark09.typingspeedtest.jwt.JWTAuthFilter;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +13,15 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
+
 @EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JWTAuthFilter jwtAuthFilter;
+
+    private final FilterChainExceptionHandler filterChainExceptionHandler;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -29,12 +32,13 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.POST, "/api/v1/difficulties/**", "/api/v1/words/**").hasAuthority("ADMIN")
                 .requestMatchers(HttpMethod.DELETE, "/api/v1/difficulties/**", "/api/v1/words/**").hasAuthority("ADMIN")
                 .requestMatchers(HttpMethod.PUT, "/api/v1/difficulties/**", "/api/v1/words/**").hasAuthority("ADMIN")
-                .requestMatchers(HttpMethod.GET,"/api/v1/difficulties/**", "/api/v1/words/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/v1/difficulties/**", "/api/v1/words/**").permitAll()
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .requestMatchers("/api/v1/auth/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(filterChainExceptionHandler, JWTAuthFilter.class)
                 .build();
 
     }
