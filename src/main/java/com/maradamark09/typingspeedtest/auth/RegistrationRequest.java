9656371b7d59c6179fb20e2e385cb705030c08b3
maradamark99 <maradamark99@gmail.com>
2023-01-18 14:56:08 +0100

    Add more clear response messages for validation errors

--- src/main/java/com/maradamark09/typingspeedtest/auth/RegistrationRequest.java
+++ src/main/java/com/maradamark09/typingspeedtest/auth/RegistrationRequest.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
-public record RegistrationRequest(@Length(min = 4, max = 50) String username,
-                                  @NotEmpty @Email(message = "Email is not valid!",
+public record RegistrationRequest(@Length(min = 4, max = 50, message =
+        "The length of the username must be between {min} and {max} characters") String username,
+                                  @NotEmpty @Email(message = "The given email: '${validatedValue}' is not a valid email.",
                                           regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$") String email,
-                                  @NotEmpty @Length(min = 8, max = 255) String password) {
+                                  @NotEmpty @Length(min = 8, max = 255, message =
+                                          "The password must be at least {min} characters long") String password) {
 
 }
