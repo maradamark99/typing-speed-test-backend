11f431df2efbb061c2ca2b17eddcf717979b2f32
maradamark99 <maradamark99@gmail.com>
2023-01-21 12:42:37 +0100

    Update constructors

--- src/main/java/com/maradamark09/typingspeedtest/exception/ResourceAlreadyExistsException.java
+++ src/main/java/com/maradamark09/typingspeedtest/exception/ResourceAlreadyExistsException.java
@@ -1,6 +1,19 @@
 package com.maradamark09.typingspeedtest.exception;
 public class ResourceAlreadyExistsException extends RuntimeException{
-    public ResourceAlreadyExistsException(String msg) {
-        super(msg);
+
+    public ResourceAlreadyExistsException(String resource) {
+        super(buildMessageWithoutValue(resource));
     }
+    public ResourceAlreadyExistsException(String resource, Object value) {
+        super(buildMessageWithValue(resource, value));
+    }
+
+    private static String buildMessageWithValue(String resource, Object value) {
+        return "The given " + resource + ": {" + value + "} already exists.";
+    }
+
+    private static String buildMessageWithoutValue(String resource) {
+        return "The given " + resource + " already exists.";
+    }
+
 }
