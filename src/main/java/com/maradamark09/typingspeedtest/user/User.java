78723e2b8223c9a58837f82f7eb5334d08c96acf
maradamark99 <maradamark99@gmail.com>
2023-01-24 16:52:32 +0100

    Map results

--- src/main/java/com/maradamark09/typingspeedtest/user/User.java
+++ src/main/java/com/maradamark09/typingspeedtest/user/User.java
@@ -1,10 +1,12 @@
 package com.maradamark09.typingspeedtest.user;
 
+import com.maradamark09.typingspeedtest.result.Result;
 import com.maradamark09.typingspeedtest.role.Role;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -32,6 +34,8 @@ public class User {
     @NotNull
     private Role role;
 
-
+    @OneToMany(mappedBy = "user")
+    @NotNull
+    private Set<Result> results;
 
 }
