cf578a7f9ead6ef6d55c44d80a632af62bd8328c
maradamark99 <maradamark99@gmail.com>
2023-01-27 17:40:50 +0100

    Make the constructors protected

--- src/main/java/com/maradamark09/typingspeedtest/exception/ResourceAlreadyExistsException.java
+++ src/main/java/com/maradamark09/typingspeedtest/exception/ResourceAlreadyExistsException.java
@@ -1,10 +1,10 @@
 package com.maradamark09.typingspeedtest.exception;
 public class ResourceAlreadyExistsException extends RuntimeException{
 
-    public ResourceAlreadyExistsException(String resource) {
+    protected ResourceAlreadyExistsException(String resource) {
         super(buildMessageWithoutValue(resource));
     }
-    public ResourceAlreadyExistsException(String resource, Object value) {
+    protected ResourceAlreadyExistsException(String resource, Object value) {
         super(buildMessageWithValue(resource, value));
     }
 
