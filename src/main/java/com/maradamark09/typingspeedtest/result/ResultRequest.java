9720ca20598cdc9d06415cf839d7913cfb418633
maradamark99 <maradamark99@gmail.com>
2023-01-27 17:41:33 +0100

    Remove userId

--- src/main/java/com/maradamark09/typingspeedtest/result/ResultRequest.java
+++ src/main/java/com/maradamark09/typingspeedtest/result/ResultRequest.java
@@ -4,6 +4,4 @@ import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Range;
 
 public record ResultRequest(@NotNull @Range(min = 0, max = 350) Short wpm,
-                            @NotNull @Range(min = 0, max = 100) Double accuracy,
-
-                            @NotNull String userId) { }
+                            @NotNull @Range(min = 0, max = 100) Double accuracy) { }
