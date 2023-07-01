cf3afa195c683077985ae7b313ebf9ea6e2ba75c
maradamark99 <maradamark99@gmail.com>
2023-07-01 10:17:17 +0200

    update exceptions

--- src/main/java/com/maradamark09/typingspeedtest/result/ResultNotFoundException.java
+++ src/main/java/com/maradamark09/typingspeedtest/result/ResultNotFoundException.java
@@ -3,14 +3,11 @@ package com.maradamark09.typingspeedtest.result;
 import com.maradamark09.typingspeedtest.exception.ResourceNotFoundException;
 
 public class ResultNotFoundException extends ResourceNotFoundException {
-
-    private final static String resource = "result";
-
     public ResultNotFoundException() {
-        super(resource);
+        super("result");
     }
 
     public ResultNotFoundException(Long id) {
-        super(resource, id);
+        super("result", id);
     }
 }
--- src/main/java/com/maradamark09/typingspeedtest/word/WordAlreadyExistsException.java
+++ src/main/java/com/maradamark09/typingspeedtest/word/WordAlreadyExistsException.java
@@ -3,9 +3,7 @@ package com.maradamark09.typingspeedtest.word;
 import com.maradamark09.typingspeedtest.exception.ResourceAlreadyExistsException;
 
 public class WordAlreadyExistsException extends ResourceAlreadyExistsException {
-
-    private static final String resource = "word";
     public WordAlreadyExistsException(String value) {
-        super(resource, value);
+        super("word", value);
     }
 }
