2d2fc65fcbe6cfee2b7e4169aac3990e0d819cce
maradamark99 <maradamark99@gmail.com>
2023-07-04 18:55:20 +0200

    Remove public access modifier from fields

--- src/test/java/com/maradamark09/typingspeedtest/difficulty/DifficultyDataProvider.java
+++ src/test/java/com/maradamark09/typingspeedtest/difficulty/DifficultyDataProvider.java
@@ -5,29 +5,29 @@ import java.util.List;
 
 class DifficultyDataProvider {
 
-	public static final long DIFFICULTY_ID = 1L;
+	static final long DIFFICULTY_ID = 1L;
 
-	public static final long DIFFICULTY_ID_TO_UPDATE = 2L;
+	static final long DIFFICULTY_ID_TO_UPDATE = 2L;
 
-	public static final DifficultyDTO VALID_DIFFICULTY_REQUEST = DifficultyDTO.builder().value("easy")
+	static final DifficultyDTO VALID_DIFFICULTY_REQUEST = DifficultyDTO.builder().value("easy")
 			.maxWordLength((byte) 10).build();
 
-	public static final DifficultyDTO INVALID_DIFFICULTY_REQUEST = DifficultyDTO.builder().value("a")
+	static final DifficultyDTO INVALID_DIFFICULTY_REQUEST = DifficultyDTO.builder().value("a")
 			.maxWordLength(null).build();
 
-	public static final DifficultyDTO DIFFICULTY_RESPONSE = DifficultyDTO
+	static final DifficultyDTO DIFFICULTY_RESPONSE = DifficultyDTO
 			.builder()
 			.id(1L)
 			.value("easy")
 			.maxWordLength((byte) 10)
 			.build();
 
-	public static final List<Difficulty> LIST_OF_DIFFICULTIES = List.of(
+	static final List<Difficulty> LIST_OF_DIFFICULTIES = List.of(
 			new Difficulty(1L, "easy", (byte) 10, Collections.emptySet()),
 			new Difficulty(2L, "medium", (byte) 20, Collections.emptySet()),
 			new Difficulty(3L, "hard", (byte) 30, Collections.emptySet()));
 
-	public static final List<DifficultyDTO> LIST_OF_DIFFICULTY_RESPONSES = List.of(
+	static final List<DifficultyDTO> LIST_OF_DIFFICULTY_RESPONSES = List.of(
 			DifficultyDTO
 					.builder()
 					.id(1L)
