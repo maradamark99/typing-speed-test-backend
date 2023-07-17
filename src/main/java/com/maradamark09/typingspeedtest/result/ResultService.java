f78834aa299b9650e85abe098aafbd1c7464f6c2
maradamark99 <maradamark99@gmail.com>
2023-07-17 22:07:50 +0200

    Update Controller and Service to return PageReponse

--- src/main/java/com/maradamark09/typingspeedtest/result/ResultService.java
+++ src/main/java/com/maradamark09/typingspeedtest/result/ResultService.java
@@ -3,6 +3,7 @@ package com.maradamark09.typingspeedtest.result;
 import com.maradamark09.typingspeedtest.exception.ResourceAlreadyExistsException;
 import com.maradamark09.typingspeedtest.exception.ResourceNotFoundException;
 import com.maradamark09.typingspeedtest.user.User;
+import com.maradamark09.typingspeedtest.util.PageResponse;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,11 +12,11 @@ import org.springframework.data.domain.PageRequest;
 
 public interface ResultService {
 
-    List<ResultDTO> getAmountOf(PageRequest pageRequest);
+    PageResponse<ResultDTO> getAmountOf(PageRequest pageRequest);
 
     List<ResultDTO> getByUserId(UUID userId) throws ResourceNotFoundException;
 
-    void save(ResultDTO resultRequest, User user) throws ResourceAlreadyExistsException;
+    void save(ResultDTO resultRequest, User user) throws ResourceAlreadyExistsException, ResourceNotFoundException;
 
     void deleteById(Long id) throws ResourceNotFoundException;
 
