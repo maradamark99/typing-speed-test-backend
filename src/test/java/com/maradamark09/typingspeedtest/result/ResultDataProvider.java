73b5b285cf95ad71a4c7d18a77a68e95b09c8039
maradamark99 <maradamark99@gmail.com>
2023-07-19 17:36:51 +0200

    refactor: update tests

--- src/test/java/com/maradamark09/typingspeedtest/result/ResultDataProvider.java
+++ src/test/java/com/maradamark09/typingspeedtest/result/ResultDataProvider.java
@@ -1,12 +1,18 @@
 package com.maradamark09.typingspeedtest.result;
 
+import com.maradamark09.typingspeedtest.difficulty.Difficulty;
 import com.maradamark09.typingspeedtest.user.User;
+import com.maradamark09.typingspeedtest.util.PageResponse;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
 class ResultDataProvider {
+
+	static final Long VALID_DATE = 1689777436000L;
 
 	static final long VALID_RESULT_ID = 1L;
 
@@ -24,9 +30,31 @@ class ResultDataProvider {
 
 	static final int INVALID_SIZE = -15;
 
-	static final ResultDTO VALID_RESULT_DTO = ResultDTO.builder().wpm((short) 65).accuracy(75.55).build();
+	static final ResultDTO VALID_RESULT_DTO = ResultDTO
+			.builder()
+			.wpm((short) 65)
+			.accuracy(75.55)
+			.difficulty("EASY")
+			.date(VALID_DATE)
+			.build();
 
-	static final ResultDTO INVALID_RESULT_DTO = ResultDTO.builder().wpm(null).accuracy(41232.23).build();
+	static final Result RESULT_ENTITY = Result
+			.builder()
+			.wpm((short) 65)
+			.accuracy(BigDecimal.valueOf(75.55))
+			.difficulty(
+					Difficulty
+							.builder()
+							.id(1L)
+							.value("EASY")
+							.maxWordLength((byte) 8)
+							.build())
+			.date(Timestamp.from(Instant.ofEpochMilli(VALID_DATE)))
+			.user(LOGGED_IN_USER)
+			.build();
+
+	static final ResultDTO INVALID_RESULT_DTO = ResultDTO.builder().wpm(null).accuracy(41232.23).difficulty("ASD")
+			.build();
 
 	static final List<Result> LIST_OF_RESULT_ENTITIES = List.of(
 			Result.builder()
@@ -37,6 +65,13 @@ class ResultDataProvider {
 							User.builder()
 									.username("john")
 									.build())
+					.difficulty(
+							Difficulty.builder()
+									.id(1L)
+									.value("EASY")
+									.maxWordLength((byte) 8)
+									.build())
+					.date(Timestamp.from(Instant.ofEpochMilli(VALID_DATE)))
 					.build(),
 			Result.builder()
 					.id(2L)
@@ -46,6 +81,13 @@ class ResultDataProvider {
 							User.builder()
 									.username("jane")
 									.build())
+					.difficulty(
+							Difficulty.builder()
+									.id(2L)
+									.value("MEDIUM")
+									.maxWordLength((byte) 12)
+									.build())
+					.date(Timestamp.from(Instant.ofEpochMilli(VALID_DATE)))
 					.build());
 
 	static final List<ResultDTO> LIST_OF_RESULT_DTO = List.of(
@@ -54,7 +96,28 @@ class ResultDataProvider {
 					.id(1L)
 					.wpm((short) 65)
 					.accuracy(89.9)
+					.difficulty("EASY")
 					.username("john")
+					.date(VALID_DATE)
+					.build(),
+			ResultDTO
+					.builder()
+					.id(2L)
+					.wpm((short) 90)
+					.accuracy(95.2)
+					.difficulty("MEDIUM")
+					.username("jane")
+					.date(VALID_DATE)
+					.build());
+
+	static final PageResponse<ResultDTO> PAGE_OF_RESULT_DTO = new PageResponse<>(List.of(
+			ResultDTO
+					.builder()
+					.id(1L)
+					.wpm((short) 65)
+					.accuracy(89.9)
+					.username("john")
+					.date(VALID_DATE)
 					.build(),
 			ResultDTO
 					.builder()
@@ -62,7 +125,9 @@ class ResultDataProvider {
 					.wpm((short) 90)
 					.accuracy(95.2)
 					.username("jane")
-					.build());
+					.date(VALID_DATE)
+					.build()),
+			1, 2, 0, true, true);
 
 	private ResultDataProvider() {
 
