4b738ed64d0eaaf9a83fe4136453caf5bd10d83c
maradamark99 <maradamark99@gmail.com>
2023-07-19 17:40:04 +0200

    Update failing tests

--- src/test/java/com/maradamark09/typingspeedtest/result/ResultServiceImplTest.java
+++ src/test/java/com/maradamark09/typingspeedtest/result/ResultServiceImplTest.java
@@ -1,13 +1,14 @@
 package com.maradamark09.typingspeedtest.result;
 
 import com.maradamark09.typingspeedtest.auth.UserNotFoundException;
+import com.maradamark09.typingspeedtest.difficulty.Difficulty;
+import com.maradamark09.typingspeedtest.difficulty.DifficultyRepository;
 import com.maradamark09.typingspeedtest.exception.ResourceNotFoundException;
 import com.maradamark09.typingspeedtest.user.UserRepository;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -16,6 +17,8 @@ import org.springframework.data.domain.PageRequest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 class ResultServiceImplTest {
@@ -28,25 +31,25 @@ class ResultServiceImplTest {
     @Mock
     private UserRepository userRepository;
 
+    @Mock
+    private DifficultyRepository difficultyRepository;
+
     private final ResultMapper mapper = new ResultMapper();
 
     @BeforeEach
     void setup() {
-        resultService = new ResultServiceImpl(mapper, resultRepository, userRepository);
+        resultService = new ResultServiceImpl(mapper, resultRepository, userRepository, difficultyRepository);
     }
 
     @Test
     public void whenGetAmountOf_andParametersInvalid_thenThrowsException() {
-
         var exception = assertThrows(IllegalArgumentException.class, () -> resultService.getAmountOf(PageRequest.of(
                 ResultDataProvider.INVALID_PAGE, ResultDataProvider.INVALID_SIZE)));
         assertThat(exception.getMessage()).contains("Page index must not be");
-
     }
 
     @Test
     public void whenGetByUserId_andUserExists_thenSuccess() {
-
         var uuid = ResultDataProvider.USER_ID;
         var expected = ResultDataProvider.LIST_OF_RESULT_DTO;
 
@@ -57,23 +60,21 @@ class ResultServiceImplTest {
         var actual = resultService.getByUserId(uuid);
 
         verify(resultRepository).findResultByUserId(uuid);
-        assertThat(actual).containsExactlyElementsOf(expected);
-
+        assertEquals(expected, actual);
     }
 
     @Test
     public void whenGetByUserId_andUserIdNotExists_thenThrowsException() {
-
         var uuid = ResultDataProvider.USER_ID;
         when(userRepository.existsById(uuid)).thenReturn(false);
-        var exception = assertThrows(UserNotFoundException.class, () -> resultService.getByUserId(uuid));
-        assertThat(exception.getMessage()).contains("The given user");
 
+        var exception = assertThrows(UserNotFoundException.class, () -> resultService.getByUserId(uuid));
+
+        assertThat(exception.getMessage()).contains("The given user");
     }
 
     @Test
     public void whenDelete_andIdExists_thenSuccess() {
-
         when(resultRepository.existsById(ResultDataProvider.VALID_RESULT_ID)).thenReturn(true);
 
         resultService.deleteById(ResultDataProvider.VALID_RESULT_ID);
@@ -83,32 +84,27 @@ class ResultServiceImplTest {
         when(resultRepository.existsById(ResultDataProvider.VALID_RESULT_ID)).thenReturn(false);
 
         assertFalse(resultRepository.existsById(ResultDataProvider.VALID_RESULT_ID));
-
     }
 
     @Test
     public void whenDelete_andIdDoesntExist_thenThrowsException() {
-
         when(resultRepository.existsById(ResultDataProvider.INVALID_RESULT_ID)).thenReturn(false);
 
         assertThrows(ResourceNotFoundException.class, () -> resultService.deleteById(ResultDataProvider.INVALID_RESULT_ID));
         verify(resultRepository, never()).deleteById(ResultDataProvider.INVALID_RESULT_ID);
-
     }
 
     @Test
     public void whenSave_withValidData_thenSuccess() {
+        var resultToSave = ResultDataProvider.VALID_RESULT_DTO;
+        var resultEntity = ResultDataProvider.RESULT_ENTITY;
 
-        resultService.save(ResultDataProvider.VALID_RESULT_DTO, ResultDataProvider.LOGGED_IN_USER);
+        when(difficultyRepository.findByValueIgnoreCase(resultToSave.getDifficulty()))
+                .thenReturn(Optional.of(Difficulty.builder().id(1L).value("EASY").maxWordLength((byte) 8).build()));
 
-        ArgumentCaptor<Result> resultCaptor = ArgumentCaptor.forClass(Result.class);
-        verify(resultRepository).save(resultCaptor.capture());
+        resultService.save(resultToSave, ResultDataProvider.LOGGED_IN_USER);
 
-        Result actual = resultCaptor.getValue();
-        assertEquals(actual.getWpm(), ResultDataProvider.VALID_RESULT_DTO.getWpm());
-        assertEquals(actual.getAccuracy().doubleValue(), ResultDataProvider.VALID_RESULT_DTO.getAccuracy());
-        assertEquals(actual.getUser(), ResultDataProvider.LOGGED_IN_USER);
-
+        verify(difficultyRepository).findByValueIgnoreCase(resultToSave.getDifficulty());
     }
 
 }
